52b433cc9b94ee88e0f97829805bed5b212d41fc
Maurizio <mauriziodm91@gmail.com>
2022-06-07 20:26:36 -0300

    created function for creating user documents with email and password

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,5 +1,10 @@
 import { useState } from 'react'
 
+import {
+  createAuthUserWithEmailAndPassword,
+  createUserDocumentFromAuth,
+} from '../../utils/firebase/firebase.utils'
+
 const defaultFormFields = {
   displayName: '',
   email: '',
@@ -11,20 +16,47 @@ const SignUpForm = () => {
   const [formFields, setFormFields] = useState(defaultFormFields)
   const { displayName, email, password, confirmPassword } = formFields
 
-  console.log(formFields)
+  //clear the form field taking the default form fields
+  const resetFormFields = () => {
+    setFormFields(defaultFormFields)
+  }
 
+  //handle function for the onSubmit ending
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+
+    if (password !== confirmPassword) {
+      alert('password do not match')
+      return
+    }
+
+    try {
+      const { user } = await createAuthUserWithEmailAndPassword(email, password)
+
+      await createUserDocumentFromAuth(user, { displayName })
+      resetFormFields()
+    } catch (error) {
+      if (error.code === 'auth/email-already-in-use') {
+        alert('Email already in use')
+      } else {
+        console.log('user creation encountered an error: ', error)
+      }
+    }
+  }
+
+  //function that handle the onChange function
   const handleChange = (event) => {
     const { name, value } = event.target
     //this will spread the fields inside the object from the state,
     //[name] will get the name of the input field since it matches with the property inside
     //the formfields object it will update it with its respective value
-    setFormFields({ ...formFields, [name]: value })
+    setFormFields({ ...formFields, [name]: value }) //this is a good object property update shorthand
   }
 
   return (
     <div>
       <h1>Sign up with your email and password</h1>
-      <form onSubmit={() => {}}>
+      <form onSubmit={handleSubmit}>
         <label>Display Name</label>
         <input
           type='text'
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -5,6 +5,7 @@ import {
   signInWithRedirect,
   signInWithPopup,
   GoogleAuthProvider,
+  createUserWithEmailAndPassword,
 } from 'firebase/auth'
 
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore'
@@ -28,18 +29,22 @@ googleProvider.setCustomParameters({
 })
 
 export const auth = getAuth()
+
 export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider)
 
 export const signInWithGoogleRedirect = () =>
   signInWithRedirect(auth, googleProvider)
 
-//instanceof firebase
+//instanceof firebase firestore
 
 export const db = getFirestore()
 
-//getting the user document from auth
+//creating the user document from auth
 
-export const createUserDocumentFromAuth = async (userAuth) => {
+export const createUserDocumentFromAuth = async (
+  userAuth,
+  additionalInformation = {}
+) => {
   const userDocRef = doc(db, 'users', userAuth.uid)
 
   const userSnapShot = await getDoc(userDocRef)
@@ -53,6 +58,7 @@ export const createUserDocumentFromAuth = async (userAuth) => {
         displayName,
         email,
         createdAt,
+        ...additionalInformation,
       })
     } catch (error) {
       console.log('error creating the user', error.message)
@@ -60,4 +66,10 @@ export const createUserDocumentFromAuth = async (userAuth) => {
   }
 
   return userDocRef
+}
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return
+
+  return await createUserWithEmailAndPassword(auth, email, password)
 }
