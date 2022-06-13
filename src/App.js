fd736e97c03a04c841de57339db587956853487a
Maurizio <mauriziodm91@gmail.com>
2022-06-13 01:07:22 -0300

    finishing authentication page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Navigation from './routes/navigation/navigation.component'
 
 import Home from './routes/home/home.component'
 
-import Signin from './routes/sign-in/sign-in.component'
+import Authentication from './routes/authentication/authentication.component'
 
 const Shop = () => {
   return <h1>the shop page</h1>
@@ -16,7 +16,7 @@ const App = () => {
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path='shop' element={<Shop />} />
-        <Route path='sign-in' element={<Signin />} />
+        <Route path='auth' element={<Authentication />} />
       </Route>
     </Routes>
   )
---
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -0,0 +1,98 @@
+import { useState } from 'react'
+
+import './sign-in-form.styles.scss'
+
+import FormInput from '../form-input/form-input.component'
+import Button from '../button/button.component'
+
+import {
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+  signInAuthUserWithEmailAndPassword,
+} from '../../utils/firebase/firebase.utils'
+
+const defaultFormFields = {
+  email: '',
+  password: '',
+}
+
+const SignInForm = () => {
+  const [formFields, setFormFields] = useState(defaultFormFields)
+  const { email, password } = formFields
+
+  //clear the form field taking the default form fields
+  const resetFormFields = () => {
+    setFormFields(defaultFormFields)
+  }
+
+  const signInWithGoogle = async () => {
+    const { user } = await signInWithGooglePopup()
+    await createUserDocumentFromAuth(user)
+  }
+
+  //handle function for the onSubmit event
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+
+    try {
+      const response = await signInAuthUserWithEmailAndPassword(email, password)
+      console.log(response)
+      resetFormFields()
+    } catch (error) {
+      switch (error.code) {
+        case 'auth/wrong-password':
+          alert('incorrect password for email')
+          break
+        case 'auth/user-not-found':
+          alert('no user associated with this email')
+          break
+        default:
+          console.log(error)
+      }
+    }
+  }
+
+  //function that handle the onChange function
+  const handleChange = (event) => {
+    const { name, value } = event.target
+    //this will spread the fields inside the object from the state,
+    //[name] will get the name of the input field since it matches with the property inside
+    //the formfields object it will update it with its respective value
+    setFormFields({ ...formFields, [name]: value }) //this is a good object property update shorthand
+  }
+
+  return (
+    <div className='sign-up-container'>
+      <h2>Already have an account?</h2>
+      <span>Sign in with your email and password</span>
+      <form onSubmit={handleSubmit}>
+        <FormInput
+          label='Email'
+          type='email'
+          required
+          onChange={handleChange}
+          name='email'
+          value={email}
+        />
+
+        <FormInput
+          label='Password'
+          type='password'
+          required
+          onChange={handleChange}
+          name='password'
+          value={password}
+        />
+
+        <div className='buttons-container'>
+          <Button type='submit'>Sign In</Button>
+          <Button type='button' buttonType='google' onClick={signInWithGoogle}>
+            Google sign in
+          </Button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default SignInForm
