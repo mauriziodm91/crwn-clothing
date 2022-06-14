c0097b77e72f6e7aca4687497c6b03afbff2382d
Maurizio <mauriziodm91@gmail.com>
2022-06-14 16:19:06 -0300

    created signout function

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,9 +1,10 @@
-import { useState } from 'react'
+import { useState, useContext } from 'react'
 
 import './sign-up-form.styles.scss'
 
 import FormInput from '../form-input/form-input.component'
 import Button from '../button/button.component'
+import { UserContext } from '../../contexts/user.context'
 
 import {
   createAuthUserWithEmailAndPassword,
@@ -21,6 +22,8 @@ const SignUpForm = () => {
   const [formFields, setFormFields] = useState(defaultFormFields)
   const { displayName, email, password, confirmPassword } = formFields
 
+  const { setCurrentUser } = useContext(UserContext)
+
   //clear the form field taking the default form fields
   const resetFormFields = () => {
     setFormFields(defaultFormFields)
@@ -37,6 +40,8 @@ const SignUpForm = () => {
 
     try {
       const { user } = await createAuthUserWithEmailAndPassword(email, password)
+
+      setCurrentUser(user)
 
       await createUserDocumentFromAuth(user, { displayName })
       resetFormFields()
