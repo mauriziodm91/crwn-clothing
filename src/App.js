1ee89a6e3062d7f6980939cc2f1cfcbbd3c782d9
Maurizio <mauriziodm91@gmail.com>
2022-07-25 16:26:58 -0300

    implemented redux for user data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,14 @@
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+
 import { Routes, Route } from 'react-router-dom'
+
+import {
+  onAuthStateChangedListener,
+  createUserDocumentFromAuth,
+} from './utils/firebase/firebase.utils'
+
+import { setCurrentUser } from './store/user/user.actions'
 
 import Navigation from './routes/navigation/navigation.component'
 
@@ -11,6 +21,18 @@ import Shop from './routes/shop/shop.component'
 import Checkout from './routes/checkout/checkout.component'
 
 const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedListener((user) => {
+      if (user) {
+        createUserDocumentFromAuth(user)
+      }
+      dispatch(setCurrentUser(user))
+    })
+    return unsubscribe
+  }, [dispatch])
+
   return (
     <Routes>
       <Route path='/' element={<Navigation />}>
--- src/index.js
+++ src/index.js
@@ -2,24 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
-import { UserProvider } from './contexts/user.context'
 import { CategoriesProvider } from './contexts/categories.context'
 import { CartProvider } from './contexts/cart.context'
 import './index.scss'
 import reportWebVitals from './reportWebVitals'
+import { Provider } from 'react-redux'
+import { store } from './store/store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <UserProvider>
+    <Provider store={store}>
+      <BrowserRouter>
         <CategoriesProvider>
           <CartProvider>
             <App />
           </CartProvider>
         </CategoriesProvider>
-      </UserProvider>
-    </BrowserRouter>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 )
 
